31e0463a09b1eb56a3be018cb26fcfe072f180a7
Alemu Takele Assefa <alemutak@hotmail.com>
2020-09-05 12:20:31 +0200

    Update testContrast.R

--- R/testContrast.R
+++ R/testContrast.R
@@ -1,16 +1,23 @@
-#' Test contrast from PIMSeq results
+#' Test contrasts from PIMSeq results
 #' 
 #' @description Tests contrasts using results obtained from PIMSeq function. This function
 #' can be called independently after fitting PIM for gene expression data.
 #' 
 #' @param pim.res PIMSeq output 
-#' @param contrasts a vector of coefficients for the linear combination of the factors 
-#' @param only.conditions a logical value If only.conditions=TRUE (default), contrast will
+#' @param contrasts a vector of coefficients for the linear combination of the factors. This should
+#' be always in a matrix class with rows for contrats and columns equal to the number of model coefficients.
+#' @param null.PI.limits NULL (default) or a numeric vector size at most 2. If not NULL, values must be within 0 and 1.
+#' This argument can be used to particularly specify the null PI to be tested (often for one sided test). 
+#' That is, if one wants to test if the true PI is less than 0.2 or greater than 0.8, then we write 
+#' null.PI.limits = c(0.2, 0.8). The dafault is NULL for the standard case that testing if the PI is 
+#' less than or greater than 0.5. It is possible to use different values (e.g. null.PI.limits = c(0.4, 0.8)).
+#' @param only.conditions a logical value. If only.conditions=TRUE (the default), contrast will
 #' be tested among the coefficients of condition variables only. If only.conditions=FALSE,
 #' contrast will be tested considering all coefiecients in the model (conditions and
 #' nuisance variables). The dimension of the contrast matrix/vector depends on this argument. 
 #' @param gene.warning.return a logical value to show warning message if the contrast 
 #' test for a particular gene encounters an error. Default is FALSE.
+#' @param verbose A logical value. If true, a message about the type of test being performed will printed.
 #' @param ... additional arguiments
 #' 
 #' 
@@ -18,16 +25,53 @@
 #' error, p value, estimated PI, and FDR adjusted p value.
 #' 
 #' @examples
-#'  # see examples of PIMSeq function.
+#'  # examples
 #' @export
 
-
-testPIMcontrast<- function(pim.res,  contrasts, only.conditions=TRUE, 
-                           gene.warning.return=FALSE, ...){
+testPIMcontrast<- function(pim.res,  contrasts, null.PI.limits = NULL, only.conditions=TRUE, 
+                           gene.warning.return=FALSE, verbose=TRUE, ...){
   if(is.null(pim.res)) stop("PIM results not found.") 
   if(is.null(contrasts)) stop("Must specify contrasts.")
   if(anyNA(contrasts))  stop("NAs not allowed in contrasts")
   if(!is.matrix(contrasts)) stop("Contrast should be a matrix class. Rows showd be contrasts and the number of columns should be equal to the number of coefficients in the model.")
+  
+  if(!is.null(null.PI.limits)){
+    if(nrow(contrasts)>1){
+      warning("The null PI is ignored.")
+    }else{
+      if(length(null.PI.limits) > 2){
+        stop("The length of null PIs must be at most 2.")
+      }
+      
+      if(any(null.PI.limits<=0) | any(null.PI.limits>=1)){
+        stop("The null PI must be within the interval (0, 1).")
+      }else{
+        #assuming a logit link was used for thr PIM result
+        if(length(null.PI.limits)==1){
+          null.value <- log(null.PI.limits/(1-null.PI.limits)) 
+           
+        }else{
+          null.value1 <- log(null.PI.limits[1]/(1-null.PI.limits[1]))
+          null.value2 <- log(null.PI.limits[2]/(1-null.PI.limits[2])) 
+        }
+      } 
+    }
+  }else{
+    null.value1 <- null.value2 <- 0
+  }
+  
+  if(verbose & !is.null(null.PI.limits)){
+    if(length(null.PI.limits)==1){
+      if(null.PI.limits>=0.5){
+        message(paste0("Testing the alternative hypothesis HA: PI > ",null.PI.limits))
+      }else{
+        message(paste0("Testing the alternative hypothesis HA: PI < ",null.PI.limits))
+      } 
+    }else{
+      message(paste0("Testing the alternative hypothesis HA: PI < ",null.PI.limits[1], " or HA: PI > ",null.PI.limits[2]))
+    } 
+  }
+  
   
   if(only.conditions){
     name.X <- pim.res$add.PIM.results$PIMSeq.inputs$condition
@@ -39,7 +83,7 @@ testPIMcontrast<- function(pim.res,  contrasts, only.conditions=TRUE,
   
   per.gene.test <- lapply(pim.res$add.PIM.results$fit.model, function(pim.res.gene){
     sub.coef <- sort(unique(do.call("c", lapply(name.X, 
-                  function(nm) grep(nm, names(pim.res.gene$b)))))) 
+                                                function(nm) grep(nm, names(pim.res.gene$b)))))) 
     
     b    <- as.matrix(pim.res.gene$b[sub.coef])
     v    <- pim.res.gene$v[sub.coef,sub.coef]
@@ -61,7 +105,7 @@ testPIMcontrast<- function(pim.res,  contrasts, only.conditions=TRUE,
         PI.DE.all <- as.numeric(exp(lf)/(1+exp(lf))) 
         PI.DE <- PI.DE.all[which.max(abs(PI.DE.all-0.5))]
         if(all(class(v.lf.inv) != "try-error")){
-          z.lf  <- as.numeric((t(lf) %*% v.lf.inv %*% lf))
+          z.lf  <- as.numeric((t(lf) %*% v.lf.inv %*% (lf)))
           p.val <- as.numeric(1-pchisq(q=z.lf, df = nrow(contrasts)))
         }else{
           z.lf  <- NA
@@ -69,7 +113,7 @@ testPIMcontrast<- function(pim.res,  contrasts, only.conditions=TRUE,
         } 
         out.list <- data.frame(ID = as.character(pim.res.gene$tag.name),
                                PI=PI.DE, Chis.quare=z.lf,  p.value=p.val)
-       
+        
         
       }else{
         if(ncol(contrasts) != length(b)) stop("Number of columns of contrast matrix must match number of coefficients in fit")
@@ -78,8 +122,33 @@ testPIMcontrast<- function(pim.res,  contrasts, only.conditions=TRUE,
         PI.DE.all <- as.numeric(exp(lf)/(1+exp(lf))) 
         PI.DE <- PI.DE.all[which.max(abs(PI.DE.all-0.5))]
         if(v.lf>0){
-          z.lf <- as.numeric(lf/sqrt(v.lf))
-          p.val <- as.numeric(2*(1-pnorm(abs(z.lf))))
+          if(!is.null(null.PI.limits)){
+            if(length(null.PI.limits)==1){
+              if(null.PI.limits>=0.5){
+                #if(!verbose){message(paste0("Testing the alternative hypothesis HA: PI > ",null.PI.limits))}
+                z.lf <- as.numeric((lf-null.value)/sqrt(v.lf))  
+                p.val <- (1- pnorm(z.lf))
+              }else{
+                #if(!verbose){message(paste0("Testing the alternative hypothesis HA: PI < ",null.PI.limits))}
+                z.lf <- as.numeric((lf-null.value)/sqrt(v.lf))  
+                p.val <- pnorm(z.lf)
+              }
+              
+            }else{
+              #if(!verbose){message(paste0("Testing the alternative hypothesis HA: PI < ",null.PI.limits[1], " or HA: PI > ",null.PI.limits[2]))}
+              z.lf1 <- as.numeric((lf-null.value1)/sqrt(v.lf))
+              z.lf2 <- as.numeric((lf-null.value2)/sqrt(v.lf))
+              z.lf <- c(z.lf1, z.lf2)
+              
+              z.lf <- z.lf[which.max(abs(z.lf))] 
+              p.val <- min(c(pnorm(z.lf1), (1- pnorm(z.lf2))))
+            } 
+          }else{
+            z.lf  <- as.numeric(lf/sqrt(v.lf))
+            p.val <- 2*(1- pnorm(abs(z.lf))) 
+          }
+          
+          
         }else{
           z.lf  <- NA
           p.val <- NA  
@@ -100,8 +169,6 @@ testPIMcontrast<- function(pim.res,  contrasts, only.conditions=TRUE,
   
   test.contrasts
 } 
-
-
 
 
 
